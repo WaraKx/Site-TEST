eae5b0146be03475842dfa06e7af985faa255f11
kyllian.bretagne <kyllian.bretagne@efrei.net>
2024-09-11 16:12:11 +0200

    Mise en page

---
+++ script.js
@@ -0,0 +1,12 @@
+// Identifier le Bouton
+const btn = document.getElementById("btn")
+
+// Définir une fonction
+btn.addEventListener("click", (e)) (
+    console.log.e.target
+)
+// Rajouter un click event
+btn.addEventListener("click", handleClick)
+
+// Log pour tester
+//console.log ("test ...", btn)
